ea634cf3874acf2d158be216a0d04b8d8a9c268f
Aditinag05 <139995488+Aditinag05@users.noreply.github.com>
2024-05-12 18:55:09 +0530

    Update.js

--- INDEX/.js
+++ INDEX/.js
@@ -1,15 +1,42 @@
-const planet = document.querySelector('.planet');
+$(window).load(function(){
 
-function movePlanet() {
-  const x = Math.random() * (window.innerWidth - 200);
-  const y = Math.random() * (window.innerHeight - 200);
+  var body = $("body"),
+      universe = $("#universe"),
+      solarsys = $("#solar-system");
 
-  planet.style.transition = 'transform 2s';
-  planet.style.transform = `translate(${x}px, ${y}px)`;
-}
+  var init = function() {
+    body.removeClass('view-2D opening').addClass("view-3D").delay(2000).queue(function() {
+      $(this).removeClass('hide-UI').addClass("set-speed");
+      $(this).dequeue();
+    });
+  };
 
-movePlanet();
+  var setView = function(view) { universe.removeClass().addClass(view); };
 
-setInterval(() => {
-  movePlanet();
-}, 5000);
+  $("#toggle-data").click(function(e) {
+    body.toggleClass("data-open data-close");
+    e.preventDefault();
+  });
+
+  $("#toggle-controls").click(function(e) {
+    body.toggleClass("controls-open controls-close");
+    e.preventDefault();
+  });
+
+  $("#data a").click(function(e) {
+    var ref = $(this).attr("class");
+    solarsys.removeClass().addClass(ref);
+    $(this).parent().find('a').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+  $(".set-view").click(function() { body.toggleClass("view-3D view-2D"); });
+  $(".set-zoom").click(function() { body.toggleClass("zoom-large zoom-close"); });
+  $(".set-speed").click(function() { setView("scale-stretched set-speed"); });
+  $(".set-size").click(function() { setView("scale-s set-size"); });
+  $(".set-distance").click(function() { setView("scale-d set-distance"); });
+
+  init();
+
+});
